be283dbce7d3da3ac219bb73feefc4d96958c77f
ymf510695949 <510695949@qq.com>
2017-08-05 14:10:26 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/yemingfeng03.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/5.
+ */
+public class yemingfeng03 {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+    }
+
+}
